1a8405142e84547c5d2b72c55edde55937a57ff8
matheusdelnero <matheusdelnerovendas@gmail.com>
2022-04-09 21:21:13 -0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,1 +1,11 @@
-impor
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run('OTYyNTAzNzAwMDIxMTI5MjM3.YlIfbA.QG8s2NmGhK9fDq_WjoFRMOQjhm0')
